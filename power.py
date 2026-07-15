3dcbcbf936f19cc48797f7dab81751c47481bc49
Jahnavi-HG <jahnavihg1405@gmail.com>
2026-07-15 22:14:01 +0530

    power of (X,n)

---
+++ power.py
@@ -0,0 +1,7 @@
+class Solution:
+    def myPow(self,x:float,n:int):
+        if (n==0):
+            return 1
+        if n<0:
+            return 1/self.myPow(x,n-1)
+        return x*self.myPow(x,n-1)
