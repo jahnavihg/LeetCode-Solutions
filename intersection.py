7d35775e6bd6fda235f582ac941027ae2cc8587e
Jahnavi-HG <jahnavihg1405@gmail.com>
2026-08-06 21:16:35 +0530

    buy or sell 2

---
+++ intersection.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getIntersectionNode(self, headA, headB):
+
+        pA = headA
+        pB = headB
+
+        while pA != pB:
+            pA = pA.next if pA else headB
+            pB = pB.next if pB else headA
+
+        return pA
